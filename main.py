3ed89a3356c139d65c8ad93c7a9b0a15563ca7cc
TLR8cheeseBro <theo.victoria9@gmail.com>
2024-11-11 22:37:10 -0500

    B. for real this time adding resolution

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 # import pygame library, dont forget the basics ya filthy animal
 import pygame
+from constants import *
 
 def main():
     print("Starting asteroids!")
+    print(f"Screen Width: {SCREEN_WIDTH}")
+    print(f"Screen Height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
