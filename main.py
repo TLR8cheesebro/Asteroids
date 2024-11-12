e28a3487bd7908c616f5725fd47e3e7fb7abf336
TLR8cheeseBro <theo.victoria9@gmail.com>
2024-11-11 23:20:52 -0500

    figuring out the rect parameter on the while screen refresh loop

--- main.py
+++ main.py
@@ -2,10 +2,24 @@
 import pygame
 from constants import *
 
+class Rect:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
 def main():
+    pygame.init()
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    On = True
+
+#Refresh screen black 
+    while On == True:
+        screen.fill(255)
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
