c4c7885342fcd5b9e17c0b84dc4e6eef5b6e7fa7
TLR8cheeseBro <theo.victoria9@gmail.com>
2024-11-11 22:42:53 -0500

    "actually changing the spelling in main.py"

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from constants import *
 
 def main():
     print("Starting asteroids!")
-    print(f"Screen Width: {SCREEN_WIDTH}")
-    print(f"Screen Height: {SCREEN_HEIGHT}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
